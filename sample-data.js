f5789b491638dcfd82f33b4409a7547c3e4b7e71
HUMARU <30289433+CHANGGOMARU@users.noreply.github.com>
2026-05-26 23:36:27 +0900

    Update sample-data.js

--- sample-data.js
+++ sample-data.js
@@ -4,5 +4,11 @@ window.MANU_SAMPLES = [
     "youtubeUrl": "https://www.youtube.com/embed/",
     "description": "VCV 방식의 일본어 발음과 음색을 확인할 수 있는 임시 샘플입니다.",
     "credit": "UTAU MANU / v0.9"
+  },
+  {
+    "title": "MANU DEMO SAMPLE",
+    "youtubeUrl": "https://www.youtube.com/embed/cZ3BUAfoOeY",
+    "description": "MANU의 0.9버전 데모 및 샘플입니다.\n\n25時の情熱 / カンザキイオリ\nORIGINAL_SONG :\nhttps://www.youtube.com/watch?v=RifILHUOV_w\nUST :\n마따따비(TABI)\n https://www.youtube.com/watch?v=xb2PCrpXU90\n\n キャットラビング / 香椎モイミ\nORIGINAL_SONG :\nhttps://www.youtube.com/watch?v=7gwvBrlUiww\nUST :\n茶鬼/ちゃき/Chaki\n\nサイバーパンクデッドボーイ / マイキ\nORIGINAL_SONG :\nhttps://www.youtube.com/watch?v=5HPJ7eUvU4o\nUST :\nNarzum\nhttps://www.youtube.com/watch?v=rAwMbitHE5M",
+    "credit": ""
   }
 ];
